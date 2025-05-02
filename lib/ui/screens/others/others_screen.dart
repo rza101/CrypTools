7768f0fb525f2b527000d602887a3993c10deaa7
rza101 <69953361+rza101@users.noreply.github.com>
2025-05-02 17:47:51 +0700

    fix: others screen button to card

--- lib/ui/screens/others/others_screen.dart
+++ lib/ui/screens/others/others_screen.dart
@@ -3,22 +3,51 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class OthersScreen extends StatelessWidget {
+  static final _borderRadius = BorderRadius.all(Radius.circular(12));
+
   const OthersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () => context.push(RoutePaths.keyDerivation),
-            child: const Text('Key Derivation'),
-          ),
-          ElevatedButton(
-            onPressed: () => context.push(RoutePaths.keygen),
-            child: const Text('Key Generation'),
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: _borderRadius),
+                child: InkWell(
+                  borderRadius: _borderRadius,
+                  onTap: () => context.push(RoutePaths.keyDerivation),
+                  child: Center(
+                    child: ListTile(
+                      leading: const Icon(Icons.transform),
+                      title: const Text('Key Derivation'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: _borderRadius),
+                child: InkWell(
+                  borderRadius: _borderRadius,
+                  onTap: () => context.push(RoutePaths.keygen),
+                  child: Center(
+                    child: ListTile(
+                      leading: const Icon(Icons.enhanced_encryption),
+                      title: const Text('Key Generation'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
